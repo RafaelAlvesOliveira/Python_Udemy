e80874d7b342cb9ec8db0e23a8bc75819c2bc6f3
RafaelAlvesOliveira <faelalvesoliveira06@gmail.com>
2024-11-24 22:29:34 -0300

    Inclusão de outros exemplos de while usando atributos, contadores com break e continue.

---
+++ Python_S1_Logica/aula39_A61.py
@@ -0,0 +1,29 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 100
+
+##########
+
+contador += 2  # Soma
+print(contador)
+
+contador -= 4  # Subtração
+print(contador)
+
+contador *= 2  # Multiplicação
+print(contador)
+
+contador /= 2  # Divisão
+print(contador)
+
+contador //= 2  # Divisão inteira
+print(contador)
+
+contador **= 2  # Potenciação
+print(contador)
+
+contador %= 2  # Módulo
+print(contador)
