431624a9778d1e3096bcb8a339840a351da26355
RafaelAlvesOliveira <faelalvesoliveira06@gmail.com>
2025-02-06 22:11:42 -0300

    Combinações, permutações e produto

---
+++ Python_S2_Intermediario/aula122_A174.py
@@ -0,0 +1,30 @@
+# Combinations, Permutations e Product - Itertools
+# Combinação - Ordem não importa - iterável + tamanho do grupo
+# Permutação - Ordem importa
+# Produto - Ordem importa e repete valores únicos
+
+from itertools import combinations, permutations, product
+
+
+def print_iter(iterator):
+    print(*list(iterator), sep='\n')
+    print()
+
+
+pessoas = [
+    'João', 'Joana', 'Luiz', 'Letícia',
+]
+
+camisetas = [
+    ['preta', 'branca'],
+    ['p', 'm', 'g'],
+    ['masculino', 'feminino', 'unissex'],
+    ['algodão', 'poliéster']
+]
+
+print_iter(combinations(pessoas, 2))
+print(*list(combinations(pessoas, 2)), sep='\n')
+
+print_iter(permutations(pessoas, 2))
+
+print_iter(product(*camisetas))
