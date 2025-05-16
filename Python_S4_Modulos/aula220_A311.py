3848f479350571df72f1410b509ba763d4881ae5
RafaelAlvesOliveira <faelalvesoliveira06@gmail.com>
2025-05-16 18:05:44 -0300

    Requests para requisições HTTP com Python

---
+++ Python_S4_Modulos/aula220_A311.py
@@ -0,0 +1,16 @@
+# requests para requisições HTTP
+# Tutorial => https://www.youtube.com/watch?v=Qd8JT0bnJGs
+
+import requests
+
+# http:// -> 80
+# https:// -> 443
+url = 'http://localhost:3000'
+response = requests.get(url)
+
+print(response.status_code)
+# print(response.headers)
+# print(response.content)
+# print(response.json())
+print(response.text)
+# print(response)
