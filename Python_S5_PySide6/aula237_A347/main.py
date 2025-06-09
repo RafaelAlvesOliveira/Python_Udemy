0798d3c1a708ecd3b264691cdba98abb462a8f77
RafaelAlvesOliveira <faelalvesoliveira06@gmail.com>
2025-06-09 11:00:18 -0300

    Inclusão da logo da calculadora e do arquivo variables.py para indicar o caminho da logo. Foram realizadas alterações nos arquivos main.py e main_window.py.

--- Python_S5_PySide6/aula237_A347/main.py
+++ Python_S5_PySide6/aula237_A347/main.py
@@ -1,17 +1,33 @@
 import sys
 
+from PySide6.QtGui import QIcon
+from variables import WINDOW_ICON_PATH
+
 from main_window import MainWindow
 from PySide6.QtWidgets import (QApplication, QLabel)
 
 
+def temp_label(texto):
+    label1 = QLabel(texto)
+    label1.setStyleSheet('font-size: 150px')
+    return label1
+
+
 if __name__ == '__main__':
+    # Cria a aplicação
     app = QApplication(sys.argv)
     window = MainWindow()
+
+    # Define o ícone
+    icon = QIcon(str(WINDOW_ICON_PATH))
+    window.setWindowIcon(icon)
+    app.setWindowIcon(icon)
+
+    # label1 = QLabel('O meu texto')
+    # label1.setStyleSheet('font-size: 150px')
+    # window.addWidgetToVLayout(label1)
+
+    # Executa tuo
     window.adjustFixedSize()
-
-    label1 = QLabel('O meu texto')
-    label1.setStyleSheet('font-size: 150px')
-    window.v_layout.addWidget(label1)
-
     window.show()
     app.exec()
---
+++ Python_S5_PySide6/aula237_A347/variables.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+ROOT_DIR = Path(__file__).parent
+FILE_DIR = ROOT_DIR / 'files'
+WINDOW_ICON_PATH = FILE_DIR / 'icon2.png'
