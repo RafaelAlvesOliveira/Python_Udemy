50922891b5b899eb3876c3028c6a1bbc08f0d300
RafaelAlvesOliveira <faelalvesoliveira06@gmail.com>
2025-06-09 10:29:29 -0300

    Calculadora: Criando a janela principal com QMainWIndow, QWidget e QVBoxLayout

---
+++ Python_S5_PySide6/aula237_A347/main_window.py
@@ -0,0 +1,20 @@
+from PySide6.QtWidgets import (QMainWindow, QVBoxLayout, QWidget)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, parent: QWidget | None = None, *args, **kwargs) -> None:
+        super().__init__(parent, *args, **kwargs)
+
+        self.cw = QWidget()
+        self.v_layout = QVBoxLayout()
+        self.cw.setLayout(self.v_layout)
+        self.setCentralWidget(self.cw)
+
+        # Título da janela
+        self.setWindowTitle('Calculadora')
+
+    def adjustFixedSize(self):
+        # Última coisa a ser feita
+        self.adjustSize()
+        # Serve para ajustar o tamanho da janela de acordo com o conteúdo
+        self.setFixedSize(self.width(), self.height())
