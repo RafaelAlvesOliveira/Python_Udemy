4b72e5233d03f127002328e09bf13d650dd29e96
Rafael Alves <faelalvesoliveira06@gmail.com>
2025-06-28 20:32:49 -0300

    Captuando teclas ENTER, Backspace e ESC, adicionando ações nas teclas C, D e sinal de igual.

--- Python_S5_PySide6/aula237_A347/display.py
+++ Python_S5_PySide6/aula237_A347/display.py
@@ -3,10 +3,13 @@ from PySide6.QtGui import QKeyEvent
 from PySide6.QtWidgets import QLineEdit
 from variables import (BIG_FONT_SIZE, SMALL_FONT_SIZE,
                        MINIMUM_WIDTH, TEXT_MARGIN)
+from utils import isEmpty
 
 
 class Display(QLineEdit):
-    eqRequested = Signal()
+    eqPressed = Signal()
+    delPressed = Signal()
+    clearPressed = Signal()
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -21,14 +24,35 @@ class Display(QLineEdit):
         self.setTextMargins(*margins)
 
     def keyPressEvent(self, event: QKeyEvent) -> None:
+        text = event.text().strip()
         key = event.key()
         KEYS = Qt.Key
 
-        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return]
+        isEnter = key in [KEYS.Key_Enter, KEYS.Key_Return, KEYS.Key_Equal]
+        isDelete = key in [KEYS.Key_Backspace, KEYS.Key_Delete, KEYS.Key_D]
+        isEsc = key in [KEYS.Key_Escape, KEYS.Key_C]
 
         if isEnter:
-            print('Enter pressionado, sinal emitido.', type(self).__name__)
-            self.eqRequested.emit()
+            print(f'EQ {text} pressionado, sinal emitido.',
+                  type(self).__name__)
+            self.eqPressed.emit()
             return event.ignore()
+
+        if isDelete:
+            print(f'isDelete {text} pressionado, sinal emitido.', type(
+                self).__name__)
+            self.delPressed.emit()
+            return event.ignore()
+
+        if isEsc:
+            print('isEsc pressionado, sinal emitido.', type(self).__name__)
+            self.clearPressed.emit()
+            return event.ignore()
+
+        # Não passar daqui se não tiver texto
+        if isEmpty(text):
+            return event.ignore()
+
+        print('Texto', text)
 
         # return super().keyPressEvent(event)
